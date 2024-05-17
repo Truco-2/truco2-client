5e343947d7c9a18289df4d5e9e55935f12b8bc86
RonaldKauan <kauanpaz49@gmail.com>
2024-05-17 00:40:46 -0400

    Atualizar o App.tsx

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React + Truco 2</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count is {count} oi Pedrovisk
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
