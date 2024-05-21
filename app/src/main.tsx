8ed065154fb7b9ce5c08895ba140c2379404d704
Ronald Paz <kauanpaz49@gmail.com>
2024-05-21 00:59:44 -0400

    wip: Implementing login

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -3,11 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Root from './routes/root.tsx';
-<<<<<<< HEAD
 import Login from './pages/Login/Login.tsx';
-=======
-import Login from './pages/Login.tsx';
->>>>>>> 735f900 (chore: Create initial route structure)
 
 const router = createBrowserRouter([
     {
