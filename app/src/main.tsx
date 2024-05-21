80c6c9529c48ee66568bf3bfff921dadab03db48
Ronald Paz <kauanpaz49@gmail.com>
2024-05-20 23:26:46 -0400

    chore: Create initial route structure

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Root from './routes/root.tsx';
+import Login from './pages/Login.tsx';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Root />,
+    },
+    {
+        path: '/login',
+        element: <Login />,
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
