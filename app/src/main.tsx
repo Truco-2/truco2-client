e6031efd07865cbf9860a8fa4f2f120faeb0051b
Rafael Silva de Medeiros <rafaelmdeirosfmm@gmail.com>
2024-05-23 01:10:32 -0400

    wip: Add create room page

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Root from './routes/root.tsx';
 import Login from './pages/Login/Login.tsx';
+import CreateRoom from './pages/Room/Create/CreateRoom.tsx';
 
 const router = createBrowserRouter([
     {
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
     {
         path: '/login',
         element: <Login />,
+    },
+    {
+        path: '/create-room',
+        element: <CreateRoom />,
     },
 ]);
 
