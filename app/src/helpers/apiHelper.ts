5cbceab229482b7b648edaf70e719d7e4a14e366
Ronald Kauan <kauanpaz49@gmail.com>
2024-06-06 00:01:18 -0400

    chore: Add project environments

---
+++ app/src/helpers/apiHelper.ts
@@ -0,0 +1,3 @@
+export const API_ROOT_PATH = import.meta.env.VITE_BASE_URL;
+export const GUEST_LOGIN_PATH = import.meta.env.VITE_GUEST_LOGIN_PATH;
+export const CREATE_ROOM_PATH = import.meta.env.VITE_CREATE_ROOM_PATH;
